07dfcc56e2816b4c3cc570292b4d557c73c2bb61
praveen kumar <praveenraja4493@gmail.com>
2026-06-17 09:32:39 +0530

    Student add page created successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 
-import React from 'react';
 import LoginPage from './pages/auth/LoginPage.jsx';
 
 function App() {
@@ -8,4 +7,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;
